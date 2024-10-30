4618a14fa28ffed2c148f0e4928677d71ac0efe5
Niko142 <baya29.03.1977@gmail.com>
2024-10-31 01:54:01 +0400

    add content

---
+++ furniture.js
@@ -0,0 +1,25 @@
+const advantageElement = document.querySelectorAll('.advantage.col.p-0');
+const lastClild = document.querySelector('.advantage.col.p-0:last-child');
+
+function clearMaxWidth() {
+    return advantageElement.forEach(elem => elem.classList.remove('max'))
+}
+
+function addMaxWidth() {
+    return advantageElement.forEach(elem => elem.classList.add('max'))
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    function handleResize() {
+        let width = document.documentElement.clientWidth;
+        (width <= 1075) ? clearMaxWidth() : addMaxWidth();
+
+        ((width >= 741 && width <= 1075) ? 
+            (lastClild.style.justifyContent = 'center', lastClild.style.display = 'flex') : 
+            (lastClild.style = '')
+        );
+    }
+
+    window.addEventListener('resize', handleResize);
+    handleResize(); // вызываем функцию сразу после ее объявления
+})
